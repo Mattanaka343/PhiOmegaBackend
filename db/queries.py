332aa2edb27fb5c4ac84b26623f36d1c83a601e1
Mattanaka343 <mattanaka434@gmail.com>
2026-05-25 14:12:23 -0600

    Queries added

--- db/queries.py
+++ db/queries.py
@@ -31,14 +31,14 @@ async def fetch_displayed_orders(status:str, date:date, min_sale:float, client:s
     async with conn.cursor() as cur:
         await cur.execute("""
         SELECT 
-            t.row_hash                          AS ID,
-            t.fecha_creacion                    AS Fecha,
-            e.estatus                           AS Estatus,
-            t.venta_mxn                         AS Venta_MXN,
-            CONCAT(o.ciudad,', ',o.estado)      AS Origen,
-            CONCAT(d.ciudad,', ',d.estado)      AS Destino,
-            c.nombre                            AS Cliente,
-            p.nombre                            AS SalesRep
+            t.row_hash                          AS id,
+            t.fecha_creacion                    AS fecha,
+            e.estatus                           AS estatus,
+            t.venta_mxn                         AS venta_mxn,
+            CONCAT(o.ciudad,', ',o.estado)      AS origen,
+            CONCAT(d.ciudad,', ',d.estado)      AS destino,
+            c.nombre                            AS cliente,
+            p.nombre                            AS sales_rep
         FROM transacciones t
         JOIN clientes c
             ON t.id_cliente = c.id
@@ -55,14 +55,14 @@ async def fetch_displayed_orders(status:str, date:date, min_sale:float, client:s
             AND (%s = 'all' OR t.fecha_creacion = %s)
             AND (%s = 'none' OR t.venta_mxn > %s)
             AND (%s = 'all' OR  c.nombre = %s)
-            AND (%s = 'all' OR p.nombre = %s)
+            AND (%s = 'all' OR p.nombre = %s); 
         """,(status,status,date,date,min_sale,min_sale,client,client,sales_rep,sales_rep))
         rows = await cur.fetchall()
-        conn.close()
+    conn.close()
 
-        return rows
+    return rows
     
-async def fetch_kpi():
+async def fetch_kpi(since: date, until:date) -> dict:
     """
     """
     conn = await get_conn()
@@ -70,8 +70,185 @@ async def fetch_kpi():
     async with conn.cursor() as cur:
         await cur.execute(
         """
-        SELECT
-            COUNT( DISTINCT)
-            
-        """
+        SELECT COUNT(DISTINCT t.id_cliente) AS unique_clients_month,
+            COUNT(CASE WHEN e.estatus = 'Delivered' OR e.estatus = 'Delivered Without POD' THEN 1 END) AS delivered_orders,
+            SUM(t.utilidad_mx) AS month_profit
+
+        FROM transacciones t
+        JOIN estatus e
+            ON t.id_estatus = e.id
+
+        WHERE t.fecha_creacion >= %s
+            AND t.fecha_cración < %s;
+        """, (since,until)
         )
+
+async def fetch_top_client_bar_chart(since: date, until: date, limit:int) -> dict:
+    """
+    """
+    conn = await get_conn()
+
+    async with conn.cursor() as cur:
+        await cur.execute(
+        """
+        SELECT SUM(t.utilidad_mxn) as utilidad,
+            c.nombre as cliente
+
+        FROM transacciones t
+        JOIN clientes c
+            ON t.id_cliente = c.id 
+
+        WHERE t.fecha_creacion >= %s
+            AND t.fecha_creacion < %s
+
+        GROUP BY c.nombre
+
+        ORDER BY utilidad DESC
+        LIMIT %s ;
+        """, (since, until, limit))
+
+        rows = await cur.fetchall()
+    
+    conn.close()
+    return rows
+
+async def fetch_status_pie_graph(since: date, until:date,limit:int) -> dict:
+    """
+    """
+
+    conn = await get_conn()
+
+    async with conn.cursor() as cur:
+        await cur.execute(
+        """
+        SELECT e.estatus    AS estatus,
+            COUNT(*)        AS total
+        FROM transacciones t
+        JOIN estatus e 
+            ON t.estatus_id = e.id
+        
+        WHERE t.fecha_creacion >= %s
+            AND t.fecha_creacion < %s
+
+        GROUP BY e.estatus
+        ORDER BY total DESC;
+        """, (since, until)
+        )
+
+        rows = await cur.fetchall()
+    
+    conn.close()
+    return rows 
+
+async def fetch_best_sales_reps(since: date, until: date, limit:int) -> dict:
+    """
+    """
+
+    conn = await get_conn()
+
+    async with conn.cursor() as cur:
+        await cur.execute(
+        """
+        SELECT SUM(t.utilidad_mxn) as utilidad,
+            u.nombre AS nombre
+
+        FROM transacciones t 
+        JOIN usuarios u
+            ON t.sales_rep_id = u.id
+        
+        WHERE t.fecha_creacion >= %s
+            AND t.fecha_creacion < %s
+        
+        GROUP BY nombre
+        ORDER BY utilidad DESC
+        LIMIT %s ;
+        """, (since,until,limit)
+        )
+
+        rows = await cur.fetchall()
+
+    conn.close()
+    return rows
+
+async def fetch_most_orders_clients(since: date, until: date, limit: int) -> dict:
+    """
+    """
+
+    conn = await get_conn()
+
+    async with conn.cursor() as cur:
+        await cur.execute(
+        """
+        SELECT COUNT(*) AS total_orders,
+            c.nombre as cliente
+
+        FROM transacciones t
+        JOIN clientes c
+            ON t.id_cliente = c.id
+
+        WHERE t.fecha_creacion >= %s
+            AND t.fecha_creacion < %s
+
+        GROUP BY cliente
+        ORDER BY total_orders DESC
+        LIMIT %s ;
+        """, (since,until,limit)
+        )
+
+        rows = await cur.fetchall()
+
+    conn.close()
+    return rows
+
+async def fetch_clients_db( order_by: str, order_how: str):
+    """
+    """
+    conn = await get_conn()
+
+    async with conn.cursor() as cur:
+        await cur.execute(
+        f"""
+        SELECT c.nombre             AS cliente,
+            COUNT(*)                AS ordenes,
+            SUM(t.utilidad_mxn)     AS utilidad
+
+        FROM transacciones t
+        JOIN clientes c
+            ON t.id_cliente = c.id
+
+        GROUP BY cliente
+        ORDER BY %s {order_how};
+        """,(order_by)
+        )
+
+        rows = await cur.fetchall()
+    
+    conn.close()
+    return rows
+
+async def fetch_clients_db(order_by: str, order_how:str):
+    """
+    """
+    conn  = await get_conn()
+
+    async with conn.cursor() as cur:
+        await cur.execute(
+        f"""
+        SELECT u.nombre         AS sales_rep,
+            COUNT(*)            AS ordenes,
+            SUM(t.utilidad_mxn) AS utilidad 
+        
+        FROM transacciones t
+        JOIN usuarios u
+            ON t.sales_rep_id = u.id
+        
+        GROUP BY sales_rep
+        ORDER BY %s {order_how};
+        """,(order_by)
+        )
+
+        rows = await cur.fetchall()
+
+    conn.close()
+    return rows
+
